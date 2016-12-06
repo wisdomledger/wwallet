3a0fd343b6700f3be7ce3b2796b4055744c7db9e
wisdomledger <xushui@LAPTOP-GOJL9O78>
2016-12-06 23:31:52 +0800

    hmac_sha512 k to K

--- src/main/java/wisdomledger/wwallet/Key.java
+++ src/main/java/wisdomledger/wwallet/Key.java
@@ -1,26 +1,68 @@
 package wisdomledger.wwallet;
 
+import java.math.BigInteger;
 import java.util.Arrays;
+
+import org.spongycastle.asn1.x9.X9ECParameters;
+import org.spongycastle.crypto.digests.SHA512Digest;
+import org.spongycastle.crypto.ec.CustomNamedCurves;
+import org.spongycastle.crypto.macs.HMac;
+import org.spongycastle.crypto.params.ECDomainParameters;
+import org.spongycastle.crypto.params.KeyParameter;
+import org.spongycastle.math.ec.ECPoint;
+import org.spongycastle.math.ec.FixedPointCombMultiplier;
 
 public class Key {
 
-	byte[] k;
-	byte[] K;
+	X9ECParameters CURVE_PARAMS = CustomNamedCurves.getByName("secp256k1");
+	ECDomainParameters CURVE = new ECDomainParameters(CURVE_PARAMS.getCurve(),
+			CURVE_PARAMS.getG(), CURVE_PARAMS.getN(), CURVE_PARAMS.getH());
+
+	// byte[] k;
+
+	BigInteger k;
+	ECPoint K;
 	byte[] chainCode;
 
 	public Key(byte[] seed) {
+		
 		super();
 		byte[] i = hmac_sha512(seed);
-		k = Arrays.copyOfRange(i, 0, 32);
-		K = Arrays.copyOfRange(i, 32, 64);
+		k = new BigInteger(1, Arrays.copyOfRange(i, 0, 32));
+
+		K = kToK(k);
+		chainCode = Arrays.copyOfRange(i, 32, 64);
+
 	}
 
-	//k to K
-	byte[] kToK(byte[] k) {
-		return null;
+	static HMac createHmacSha512Digest(byte[] key) {
+		
+		SHA512Digest digest = new SHA512Digest();
+		HMac hMac = new HMac(digest);
+		hMac.init(new KeyParameter(key));
+		return hMac;
 	}
-	
-	byte[] hmac_sha512(byte[] seed){
-		return null;
+
+	static byte[] hmacSha512(HMac hmacSha512, byte[] input) {
+		
+		hmacSha512.reset();
+		hmacSha512.update(input, 0, input.length);
+		byte[] out = new byte[64];
+		hmacSha512.doFinal(out, 0);
+		return out;
 	}
+
+	// k to K
+	ECPoint kToK(BigInteger k) {
+
+		ECPoint K = new FixedPointCombMultiplier().multiply(CURVE.getG(), k);
+		return K;
+	}
+
+	byte[] hmac_sha512(byte[] seed) {
+
+		return hmacSha512(createHmacSha512Digest("Bitcoin seed".getBytes()),
+				seed);
+	}
+
 }
