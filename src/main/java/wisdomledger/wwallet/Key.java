a181e49362cc2228b4fb6b276e68f1f84244e1b5
wisdomledger <xushui@LAPTOP-GOJL9O78>
2016-12-06 21:00:03 +0800

    Updated

--- src/main/java/wisdomledger/wwallet/Key.java
+++ src/main/java/wisdomledger/wwallet/Key.java
@@ -1,19 +1,26 @@
 package wisdomledger.wwallet;
 
+import java.util.Arrays;
+
 public class Key {
 
-	byte[] seed;
 	byte[] k;
 	byte[] K;
 	byte[] chainCode;
 
 	public Key(byte[] seed) {
 		super();
-		this.seed = seed;
+		byte[] i = hmac_sha512(seed);
+		k = Arrays.copyOfRange(i, 0, 32);
+		K = Arrays.copyOfRange(i, 32, 64);
 	}
 
 	//k to K
 	byte[] kToK(byte[] k) {
+		return null;
+	}
+	
+	byte[] hmac_sha512(byte[] seed){
 		return null;
 	}
 }
