ae16a298e2be842afcc34486afb877f59eb27a3b
shuijian <shuijian.xu@outlook.com>
2016-12-06 13:59:26 +0800

    First upload

---
+++ src/main/java/wisdomledger/wwallet/App.java
@@ -0,0 +1,13 @@
+package wisdomledger.wwallet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
