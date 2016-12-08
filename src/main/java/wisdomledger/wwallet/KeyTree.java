5af3c848accd6afe25d64e43eaa7a3bef23e0b72
wisdomledger <xushui@LAPTOP-GOJL9O78>
2016-12-09 00:16:09 +0800

    KeyTree constructor type -child brother storage structure height

--- src/main/java/wisdomledger/wwallet/KeyTree.java
+++ src/main/java/wisdomledger/wwallet/KeyTree.java
@@ -2,5 +2,34 @@ package wisdomledger.wwallet;
 
 public class KeyTree {
 
-	
+	Key key;
+	KeyTree child;
+	KeyTree brother;
+
+	public KeyTree(Key key, KeyTree child, KeyTree brother) {
+		super();
+		this.key = key;
+		this.child = child;
+		this.brother = brother;
+	}
+
+	int height(KeyTree kt) {
+
+		int m, max = 0;
+		if (kt.child == null) {
+			return 0;
+		} else if (kt.brother == null) {
+			return 1;
+		} else {
+			KeyTree p = kt.child;
+			while (p != null) {
+				m = height(p);
+				if (m > max) {
+					max = m;
+				}
+				p = p.brother;
+			}
+			return max + 1;
+		}
+	}
 }
