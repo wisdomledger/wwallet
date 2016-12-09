eb897efdd53a564c186122d3e8589322d372cd7c
shuijian <shuijian.xu@outlook.com>
2016-12-09 18:55:10 +0800

    function addChild and addBrother

--- src/main/java/wisdomledger/wwallet/KeyTree.java
+++ src/main/java/wisdomledger/wwallet/KeyTree.java
@@ -13,6 +13,18 @@ public class KeyTree {
 		this.brother = brother;
 	}
 
+	Key addChild(KeyTree kt, Key key) {
+
+		while (kt.child != null) {
+			kt = kt.child;
+		}
+		return null;
+	}
+
+	Key addBrother(KeyTree kt, Key key) {
+		return null;
+	}
+
 	int height(KeyTree kt) {
 
 		int m, max = 0;
